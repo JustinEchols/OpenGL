7935787bb04838096a8b8695bf4ba73730a167a3
Justin Echols <justindawsonechols@gmail.com>
2023-08-09 21:36:23 -0500

    added an src file option when compiling from the command line

--- src/build.bat
+++ src/build.bat
@@ -3,6 +3,8 @@
 REM TODO(Justin): The need to input the assimp dll
 REM TODO(Justin): Ignore warnings on external dependencies
 
+REM NOTE(Justin): To compile demo.cpp, change the main_file to demo.cpp.
+set main_file=demo_explode.cpp
 set common_compiler_flags=-Od -nologo -W4 -Zi -wd4100 -wd4005 -wd4201 -wd4800 -wd4244 -wd4459 -wd4505 -DGLEW_STATIC=1 -D_CRT_SECURE_NO_WARNINGS=1
 set common_linker_flags=-incremental:no gdi32.lib user32.lib shell32.lib msvcrt.lib opengl32.lib winmm.lib glew32s.lib glfw3.lib assimp-vc142-mtd.lib
 set include_directories= /I "../dependencies/assimp/include" /I "../dependencies/GLFW64/include" /I "../dependencies/GLEW/include" /I "../dependencies/glm"
@@ -11,6 +13,6 @@ set lib_directories= /LIBPATH:"../dependencies/GLFW64/lib-vc2019" /LIBPATH:"../d
 IF NOT EXIST ..\build mkdir ..\build
 pushd ..\build
 
-cl ..\src\application.cpp %common_compiler_flags% %include_directories%  /link /NODEFAULTLIB:"LIBCMT" %lib_directories% %common_linker_flags% 
+cl ..\src\%main_file%  %common_compiler_flags% %include_directories%  /link /NODEFAULTLIB:"LIBCMT" %lib_directories% %common_linker_flags% 
 popd
 
