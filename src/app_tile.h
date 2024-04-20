e09b572fa0f27f4f39b760e1858978a5ec444084
Justin Echols <justindawsonechols@gmail.com>
2024-04-20 16:09:31 -0500

    app_tile.h

---
+++ src/app_tile.h
@@ -0,0 +1,34 @@
+#if !defined(APP_TILE_H)
+
+struct tile_chunk
+{
+	u32 *Tiles;
+
+};
+
+struct world
+{
+	u32 ChunkShift;
+	u32 ChunkMask;
+	s32 ChunkDim;
+
+	s32 ChunkCountX;
+	s32 ChunkCountZ;
+
+	f32 TileSideInMeters;
+	f32 ChunkDimInMeters;
+
+	tile_chunk *TileChunks;
+};
+
+struct chunk_tile_position
+{
+	s32 ChunkX;
+	s32 ChunkZ;
+
+	s32 TileX;
+	s32 TileZ;
+};
+
+#define APP_TILE_H
+#endif
